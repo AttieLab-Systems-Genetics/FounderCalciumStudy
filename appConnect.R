32479253cf9f892a30a8699b2194009e47567723
Brian Yandell <byandell@wisc.edu>
2023-08-24 10:38:07 -0500

    add appConnect

---
+++ appConnect.R
@@ -0,0 +1,33 @@
+devtools::install_cran("plotly") #  not yet on UW dataviz
+devtools::install_cran("markdown") #  not yet on UW dataviz
+devtools::install_cran("cowplot") #  not yet on UW dataviz
+devtools::install_cran("ggdendro") #  not yet on UW dataviz
+#devtools::install_github("byandell/foundr")
+
+traitData <- readRDS("traitData.rds")
+traitStats <- readRDS("traitStats.rds")
+traitSignal <- readRDS("traitSignal.rds")
+customSettings <- list(
+  help = "help.md",
+  dataset = c(
+    calcium = "Calcium traits",
+    protein = "Islet proteins",
+    basal = "Basal calcium"))
+
+################################################################
+
+title <- "Islet Calcium Study"
+
+ui <- foundr::ui(title)
+
+server <- function(input, output, session) {
+  
+  foundr::server(input, output, session,
+                 traitData, traitSignal, traitStats,
+                 customSettings)
+  
+  # Allow reconnect with Shiny Server.
+  session$allowReconnect(TRUE)
+}
+
+shiny::shinyApp(ui = ui, server = server)
